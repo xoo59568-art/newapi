a57e46c502d9cde8552e44464cef3aa9b1a8a81c
xoo59568-art <xoo59568@gmail.com>
2026-07-09 16:33:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const io = new Server(server, {
 
 global.botSockets = new Map();
 let SERVER_COUNT = 0;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.set("trust proxy", true);
 
